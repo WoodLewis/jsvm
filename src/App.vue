df334a9df2ceca1e30458c32f8116f6af6cd10f5
lewis <631431929@qq.com>
2022-03-23 16:57:49 +0800

    添加说明

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <span>
+      这是使用纯js实现的一个js编译&amp;解释器，用于在没有webassenbly环境的情况下加密关键前端代码。作为虚拟机上的解释器，当然性能就不要指望了，最好不要用来做大计算量的任务。<br>
+      在左侧编辑器输入要加密的js代码，点击转换，会在右侧编辑器显示出加密后的代码，点击运行会执行一遍编译后的代码。
+    </span>
+    <br>
     <button @click="compileMd5">编译md5</button>
     <button @click="compileSha512">编译sha512</button>
     <div style="display:flex">
